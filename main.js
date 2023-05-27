aabbc9d149463ce2cbd83fd5032d04d5e5cf4ca9
Alan Augusto <allanaugusto18@gmail.com>
2023-05-27 01:12:06 -0300

    Exercicio 4 finalizado

--- main.js
+++ main.js
@@ -14,15 +14,25 @@
         Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
         💡 Para saber o tipo de dado você pode usar o operador `typeof`
 */
-    let variableA = 1;
-    let variableB = 'teste';
+    //let variableA = 1;
+    //let variableB = 'teste';
 
-    console.log(`1º Caso: ${!isNaN(variableA) ? 'É um número' : 'Não é um número'}`);
-    console.log(`2º Caso: ${!isNaN(variableB) ? 'É um número' : 'Não é um número'}`);
+    //console.log(`1º Caso: ${!isNaN(variableA) ? 'É um número' : 'Não é um número'}`);
+    //console.log(`2º Caso: ${!isNaN(variableB) ? 'É um número' : 'Não é um número'}`);
 
 /*
-    4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+    4. Crie um script que declare uma variável e verifique se o seu valor é uma string. 
+    Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
 */
+    const list = [ 1, 'teste', null, true, undefined];
+
+    function isString(text) {
+        return typeof text === 'string' ? 'É uma string' : 'Não é uma string';
+    }
+
+    for (const item of list) {
+        console.log(`${item}: ${isString(item)}`);        
+    }
 
 /*
     5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
