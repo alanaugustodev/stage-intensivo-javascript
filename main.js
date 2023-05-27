daf0bdaf1ba8df4110bdf9eee5a480584fe45252
Alan Augusto <allanaugusto18@gmail.com>
2023-05-27 00:47:04 -0300

    Exercicio 3 concluido

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 /*
     1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
 */
-    let message = 'Hello World!'
-    alert(message)
+    //let message = 'Hello World!'
+    //alert(message)
 /*
     2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
 */
-    let valorA = 10
-    let valorB = 20
-    console.log(valorA + valorB)
+    //let valorA = 10
+    //let valorB = 20
+    //console.log(valorA + valorB)
 /*
-    3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
-    
-    💡 Para saber o tipo de dado você pode usar o operador `typeof`
-
+    3. Crie um script que declare uma variável e verifique se o seu valor é um número.
+        Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+        💡 Para saber o tipo de dado você pode usar o operador `typeof`
 */
+    let variableA = 1;
+    let variableB = 'teste';
+
+    console.log(`1º Caso: ${!isNaN(variableA) ? 'É um número' : 'Não é um número'}`);
+    console.log(`2º Caso: ${!isNaN(variableB) ? 'É um número' : 'Não é um número'}`);
 
 /*
     4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
