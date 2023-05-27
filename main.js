700f5f23b51467fe7ccc0acb49860aef50f845e3
Alan Augusto <allanaugusto18@gmail.com>
2023-05-27 11:34:46 -0300

    Exercicio 5 realizado

--- main.js
+++ main.js
@@ -19,25 +19,32 @@
 
     //console.log(`1º Caso: ${!isNaN(variableA) ? 'É um número' : 'Não é um número'}`);
     //console.log(`2º Caso: ${!isNaN(variableB) ? 'É um número' : 'Não é um número'}`);
-
 /*
     4. Crie um script que declare uma variável e verifique se o seu valor é uma string. 
     Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
 */
-    const list = [ 1, 'teste', null, true, undefined];
+    //const list = [ 1, 'teste', null, true, undefined];
 
-    function isString(text) {
-        return typeof text === 'string' ? 'É uma string' : 'Não é uma string';
+    //function isString(text) {
+    //    return typeof text === 'string' ? 'É uma string' : 'Não é uma string';
+    //}
+
+    //for (const item of list) {
+    //    console.log(`${item}: ${isString(item)}`);        
+    //}
+/*
+    5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. 
+    Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+*/
+    const list = [1, 'teste', true, null, undefined, false];
+
+    function isBool(value){
+        return typeof value === 'boolean' ? 'É um booleano' : 'Não é um booleano';
     }
 
     for (const item of list) {
-        console.log(`${item}: ${isString(item)}`);        
+        console.log(`${item}: ${isBool(item)}`);
     }
-
-/*
-    5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
-*/
-
 /*
     6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
 */
