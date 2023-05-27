aae6951317ace561ecc07c896d21042bd0b59dc6
Alan Augusto <allanaugusto18@gmail.com>
2023-05-27 11:44:51 -0300

    Exercicio 8 realizado

--- main.js
+++ main.js
@@ -60,7 +60,9 @@
 /*
     8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
 */
-
+    //let valorA = 2
+    //let valorB = 1
+    //console.log(valorA / valorB)
 /*
     9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
 */
