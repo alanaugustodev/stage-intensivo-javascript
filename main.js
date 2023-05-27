f971d727a5cc1b7e0761ccd80c9d2f02a38e9ad8
Alan Augusto <allanaugusto18@gmail.com>
2023-05-27 11:50:05 -0300

    Exercicio 9 realizado

--- main.js
+++ main.js
@@ -64,9 +64,12 @@
     //let valorB = 1
     //console.log(valorA / valorB)
 /*
-    9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+    9. Crie um script que declare uma variável e verifique se o seu valor é um número par. 
+    Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
 */
-
+    //let valor = 2;
+    //let isEven = valor % 2 === 0 ? 'É um número par' : 'Não é um número par';
+    //console.log(isEven);
 /*
     10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar". 
 */
